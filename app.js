74d35ea990f6cf6f6e4df268c8f9621215c4d62b
Felicia Walcott <59152656+fwalcott@users.noreply.github.com>
2020-07-26 19:03:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+var userName = prompt('Hello, My name is Felicia. What is your name?'); 
+console.log('Hello' + userName + 'Welcome to my about me page!!!'); 
+alert('Hello' + userName + 'Welcome to my  about me page!!!'); 
+
+
+var questionOne = prompt(userName  + 'Do I like spicy food? Yes or No'); 
+console.log(userName + 'Do I like spicy food? Yes or No');  
+questionOne.toLowerCase() 
+
+
+if (questionOne !=='yes'  && questionOne ==='y'){
+  console.log(userName  + 'you are correct!');
+  alert(userName  + 'you are correct!');
+} else { (questionOne ==='no' && questionOne ==='n') 
+  console.log(userName  + 'Sorry but I do like spicy food'); 
+  alert(userName + 'Sorry but I do like spicy food');
+}
+
+var questionTwo = prompt(userName  + 'Do I The Fast and the Furious Movies? Yes or No'); 
+console.log(userName  + 'Do I The Fast and the Furious Movies? Yes or No'); 
+questionTwo.toLowerCase()
+
+if (questionTwo !=='yes'  && questionTwo ==='y') {
+  console.log(userName  + 'you are correct!'); 
+  alert(userName  + 'you are correct!');
+} else { (questionTwo ==='no' && questionTwo ==='n')
+  console.log(userName  + 'Sorry but I do! Even if they are insane!'); 
+  alert(userName  + 'Sorry but I do! Even if they are insane!'); 
+}
+
+
+
+var questionThree = prompt(userName  + 'Am I in Gex X'); 
+console.log(userName  + 'Am I in Gen X'); 
+questionThree.toLowerCase()
+
+if (questionThree !=='yes'  && questionThree ==='y') {
+  console.log(userName  + 'Sorry! Gen Y forever!'); 
+  alert(userName + 'Sorry! Gen Y forever!');
+} else { (questionThree ==='no' && questionThree ==='n')
+  console.log( userName  + 'You Are Right!'); 
+  alert(userName  + 'You Are Right!'); 
+} 
+var userName = alert('I hope you like my page' +  userName); var
+
